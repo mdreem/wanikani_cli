61bcac82ca4f4c392c452ac9ccba67cba718c3e0
Marc Schiereck <mdreem@fastmail.fm>
2020-11-08 19:45:13 +0100

    fix increase_version script

--- .github/scripts/increase_version.sh
+++ .github/scripts/increase_version.sh
@@ -2,21 +2,31 @@ set -eux
 git fetch --tags || echo "could not fetch tags"
 latest_version=$(git describe --tags --match "[0-9]*.[0-9]*.[0-9]*" --abbrev=0)
 
-current_branch=$(git branch --show-current)
+merged_message=$(git log -1 | grep "Merge pull request .* from mdreem/.*" -o) \
+    || (echo "not a properly formatted merge commit" && exit 0)
 
-if [[ $current_branch != feature/* ]] && [[ $current_branch != patch/* ]];
+if [[ "${merged_message}" =~ Merge\ pull\ request\ .*\ from\ mdreem/(.*) ]];
+then
+    merged_branch=${BASH_REMATCH[1]}
+    echo "merged branch: ${merged_branch}"
+else
+    echo "not a properly formatted merge commit"
+    exit 0
+fi
+
+if [[ $merged_branch != feature/* ]] && [[ $merged_branch != patch/* ]];
 then
     echo "no feature or patch branch found"
     exit 0
 fi
 
-if [[ $current_branch == feature/* ]];
+if [[ $merged_branch == feature/* ]];
 then
     echo "increasing minor version"
     new_version=$(python3 .github/scripts/version.py ${latest_version} --minor)
 fi
 
-if [[ $current_branch == patch/* ]];
+if [[ $merged_branch == patch/* ]];
 then
     echo "increasing patch version"
     new_version=$(python3 .github/scripts/version.py ${latest_version} --patch)
